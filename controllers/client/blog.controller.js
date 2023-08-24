c636c621f86d37c900b13e19fb2c32ea70a94f6a
Kdlprabin <prabinkandel444@gmail.com>
2023-08-24 10:22:14 +0545

    feat: blogs and drafts count api

--- controllers/client/blog.controller.js
+++ controllers/client/blog.controller.js
@@ -20,7 +20,27 @@ const getBlog = async (req, res) => {
     }
 }
 
+const getBlogCount = async (req, res) => {
+    try {
+        const count = await Blog.countDocuments({ status: 'published' });
+        res.status(200).json(count);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+const getDraftCount = async (req, res) => {
+    try {
+        const count = await Blog.countDocuments({ status: 'draft' });
+        res.status(200).json(count);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
 module.exports = {
     getBlogs,
-    getBlog
+    getBlog,
+    getBlogCount,
+    getDraftCount
 }
